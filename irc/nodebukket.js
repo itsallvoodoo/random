8ae27e75b66f4cb099811293e9b627a1f0e6a9e1
itsallvoodoo <chaddhobbs@gmail.com>
2014-03-28 14:56:04 -0400

    more error handling

--- irc/nodebukket.js
+++ irc/nodebukket.js
@@ -36,50 +36,51 @@ var bot = new irc.Client(config.server, config.botName, {
 * Handlers
 *  ----------------------------------------------------------------------------------------
 */
-// Error handling
-bot.addListener('error', function(message) {
-	console.log('error: ', message);
-});
+
+try {
+	// Error handling
+	bot.addListener('error', function(message) {
+		throw message;
+	});
 
 
-// JOINS
-bot.addListener("join", function(channel, who) {
-	// Welcome them in!
-	bot.say(channel, who + "...dude...welcome back!");
-});
+	// JOINS
+	bot.addListener("join", function(channel, who) {
+		// Welcome them in!
+		bot.say(channel, who + "...dude...welcome back!");
+	});
 
 
-// TEST MESSAGE RESPONSE
-bot.addListener("message", function(from, to, text, message) {
-	if (text == "test") {
-		bot.say(config.channels[0], "This is a test");
-	}
+	// TEST MESSAGE RESPONSE
+	bot.addListener("message", function(from, to, text, message) {
+		if (text == "test") {
+			bot.say(config.channels[0], "This is a test");
+		}
 
-	if (text == "message arguments") {
-		bot.say(config.channels[0], "The following is what was recieved.");
-		bot.say(config.channels[0], "prefix: " + message.prefix);
-		bot.say(config.channels[0], "nick: " + message.nick);
-		bot.say(config.channels[0], "user: " + message.user);
-		bot.say(config.channels[0], "host: " + message.host);
-		bot.say(config.channels[0], "server: " + message.server);
-		bot.say(config.channels[0], "rawCommand: " + message.rawCommand);
-		bot.say(config.channels[0], "command: " + message.command);
-		bot.say(config.channels[0], "commandType: " + message.commandType);
-	}
+		if (text == "message arguments") {
+			bot.say(config.channels[0], "The following is what was recieved.");
+			bot.say(config.channels[0], "prefix: " + message.prefix);
+			bot.say(config.channels[0], "nick: " + message.nick);
+			bot.say(config.channels[0], "user: " + message.user);
+			bot.say(config.channels[0], "host: " + message.host);
+			bot.say(config.channels[0], "server: " + message.server);
+			bot.say(config.channels[0], "rawCommand: " + message.rawCommand);
+			bot.say(config.channels[0], "command: " + message.command);
+			bot.say(config.channels[0], "commandType: " + message.commandType);
+		}
 
-	
+		
 
-});
+	});
 
-// KICKS
-bot.addListener("kick", function(channel, who, by, reason, message) {
-	// Send them on their way
-	bot.say(channel, "GTFO " + who + "!!!");
-	bot.say(channel, reason + " is a shitty way to go...");
-});
+	// KICKS
+	bot.addListener("kick", function(channel, who, by, reason, message) {
+		// Send them on their way
+		bot.say(channel, "GTFO " + who + "!!!");
+		bot.say(channel, reason + " is a shitty way to go...");
+	});
 
-// This displays all message arguments, just to test
-bot.addListener("message", function(from, to, text, message) {
-
-	
-});
+}
+catch(err) {
+	console.log(err.message);
+}
